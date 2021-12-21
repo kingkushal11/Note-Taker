c6ad364e061d43d656ab71c5df94944ae7747cf3
Kushal Khattri <kushalk62@gmail.com>
2021-12-20 22:08:53 -0800

    done with app.get

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const path = require('path');
 const fs = require("fs");
-const Notes = require('./db/sb.json');
+const Notes = require('./Develop/db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('./Develop/public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+);
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
+);
+app.get("/api/notes", (req, res) => {
+    res.json(Notes);
+});
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);

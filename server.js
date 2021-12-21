b6a5173fcdc127b4343178f79de590eadef80957
Kushal Khattri <kushalk62@gmail.com>
2021-12-21 15:56:03 -0800

    added npm uuid, done with app.post

--- server.js
+++ server.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const path = require('path');
 const fs = require("fs");
-const Notes = require('./Develop/db/db.json');
+const notes = require('./db/db.json');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
 
-app.use(express.static('./Develop/public'));
+
+app.use(express.static('./public'));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 app.get("/api/notes", (req, res) => {
-    res.json(Notes);
+    res.json(notes);
 });
 
+app.post("/api/notes", (req, res) => {
+    const addNote = req.body ;
+    addNote.id = uuidv4()
+    notes.push(addNote);
+    fs.writeFileSync(path.join(__dirname,'./db/db.json'), JSON.stringify(notes));
+    res.json(notes);
+
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id
+
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
